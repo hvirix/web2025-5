8d47a7a80b87e3b78fcf65aaacb1d4110c3af9c1
hvirix web2025-5 <sofiaasuk3@gmail.com>
2025-05-05 14:51:59 +0300

    second part

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { program } = require ('commander');
 const fs = require ('fs');
 const http = require ('http');
 const path = require ('path');
+const {image} = require("superagent/lib/node/parsers");
 
 program
     .option('-h, --host <address>')
@@ -28,18 +29,47 @@ if (!options.host) {
     return;
 }
 
-const requestListener = function (req, res) {
+const requestListener = async function (req, res) {
     const url = req.url;
     const filePath = path.join(options.cache, `${url}.jpg`);
 
-    switch(req.method) {
+    switch (req.method) {
         case 'GET':
-            fs.promises.readFile(filePath)
-                .then(image => {
-                    res.end(image);
-                })
-                .catch(err => {});
+            let image
+            try{
+                image = await fs.promises.readFile(filePath);
+                res.setHeader('Content-Type', 'image/jpeg');
+                res.statusCode = 200;
+                res.end(image);
+            }catch(err){
+                if (err.code === 'ENOENT') {
+                    if (!image) {
+                        res.statusCode = 404;
+                        res.end();
+                    }
+                }
+            }
 
+
+            break;
+        case 'PUT':
+            const chunks = [];
+            req.on('data', chunk => chunks.push(chunk));
+            req.on('end', async () => {
+                const imageData = Buffer.concat(chunks);
+                await fs.promises.writeFile(filePath, imageData);
+                res.statusCode = 201;
+                res.end();
+            });
+            break;
+        case 'DELETE':
+            fs.promises.unlink(filePath);
+            res.statusCode = 200;
+            res.end();
+            break;
+        default:
+            res.statusCode = 405;
+            res.end();
     }
 
 }
